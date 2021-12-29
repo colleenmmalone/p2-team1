8327b4477421c553d245ed60fc659dd64b8d470d
HayesCarter94 <hayecarter42@gmail.com>
2021-12-29 18:15:59 -0500

    hello

--- src/main/java/com/p2/dao/LoginsDAO.java
+++ src/main/java/com/p2/dao/LoginsDAO.java
@@ -1,5 +1,25 @@
 package com.p2.dao;
 
-public class LoginsDAO {
+import com.p2.model.Logins;
 
+public class LoginsDAO {
+	
+	private static LoginsDAO loginsDAO;
+	private LoginsDAO() {
+		super();
+	}
+
+	
+	public static LoginsDAO getLoginsDAO() {
+		if (loginsDAO == null) {
+			loginsDAO = new LoginsDAO();
+		}
+		return loginsDAO;
+	}
+	
+	public Logins findByUserId(int id) {
+		
+		}
+	
+	
 }
