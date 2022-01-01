62765a89b5189e5d22d6bc059268d872413b5bcb
stitchineer <stitchineer@yahoo.com>
2022-01-01 13:20:35 -0500

    added main, navBar, pageTitle divs

---
+++ the-cookie-jar/src/components/navBar.js
@@ -0,0 +1,18 @@
+
+
+export default function NavBar() {
+  return (
+    <div className="App" onLoad="title()" >
+     <a href="">Home</a>&emsp;
+     <a href="">Store</a>&emsp;
+     <a href="">About</a>&emsp;
+     <a href="">Settings</a>&emsp;
+    </div>
+  );
+}
+
+
+function title(){
+    var head = document.getElementById("pageTitle");
+    head.innerHTML = "hello";
+}
--- the-cookie-jar/src/index.js
+++ the-cookie-jar/src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavBar from './components/navBar'
 import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NavBar />
+  </React.StrictMode>,
+  document.getElementById('navBar')
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('main')
 );
 
 // If you want to start measuring performance in your app, pass a function
