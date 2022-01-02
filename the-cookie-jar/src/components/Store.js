9ddc958a9e7e1dac745a3c5c99ac202e639ad9a6
stitchineer <mjollymalone@mjollymalone.fios-router.home>
2022-01-02 11:11:52 -0500

    trying out image upload component

--- the-cookie-jar/src/components/Store.js
+++ the-cookie-jar/src/components/Store.js
@@ -1,13 +1,30 @@
+import React, { useState } from "react";
+import UploadAndDisplayImage from "./UploadAndDisplay";
+
+
 export default function Store(){
+    const pics = importPics(require.context('./pictures', false, /\.(png|jpe?g|svg)$/));
+
     return(
         <>
         <h3>Store</h3>
+        <img src={pics[`cake_matcha.jpg`]} class="thumb" />
         <p>when an EMPLOYEE logs in, there should be an additional button that allows them to update invetory</p>
         <p>hello this is the Store! Please buy lots of cookies</p>
 
         this is where the inventory should be called and parsed into tables<br/>
         should have image, name, price, add-to-cart button
-        
+        <p>the following upload function works, but we need to find a place to store our files</p>
+        <p>maybe a bucket on AWS?</p>
+        <UploadAndDisplayImage/>
+
         </>
     )
 }
+
+function importPics(r){
+        let pics = {};
+         r.keys().forEach((item, index) => { pics[item.replace('./', '')] = r(item); });
+        return pics
+       }
+    
